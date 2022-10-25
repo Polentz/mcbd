b930d1174af0bfa93a21a501c3cd693f3b8266a5
Giulia Polenta <giuliapolenta@gmail.com>
2022-10-25 16:39:47 +0200

    test animation with sessionStorage

--- animation.js
+++ animation.js
@@ -1,82 +1,53 @@
-// const anchorHome = document.getElementById("go-home")
 const projectShape = document.querySelectorAll(".project");
 const logo = document.querySelector(".logo-init");
 const logoNameFirst = document.getElementById("mcbd");
 const logoNameSecond = document.getElementById("architectes");
 const cover = document.querySelector(".animation-cover");
 
-// window.addEventListener("load", () => {
-//     if (typeof (localStorage.getItem("animation")) != null && localStorage.getItem("animation") != "true") {
 
-//         logo.style.display = "none";
-//         cover.style.display = "none";
 
-//         for (let i = 0; i < projectShape.length; i++) {
-//             const el = projectShape[i];
-//             el.classList.remove("animation");
-//         }
+// const urlParams = new URLSearchParams(window.location.href);
+// alert(urlParams.getAll("loaded"));
 
-//     } else {
-//         setTimeout(() => {
-//             logoNameFirst.style.transform = "translateX(0)";
-//         }, 800);
 
-//         setTimeout(() => {
-//             logoNameSecond.style.transform = "translateX(0)";
-//         }, 800);
+const animation = sessionStorage.getItem("animation");
 
-//         for (let i = 0; i < projectShape.length; i++) {
-//             const el = projectShape[i];
-//             el.classList.add("animation");
-//             el.classList.add("animation");
-//             setTimeout(() => {
-//                 el.classList.remove("animation");
-//             }, 1800);
-//             localStorage.setItem("animation", "true");
-//         }
-
-//         setTimeout(() => {
-//             cover.style.backgroundColor = "transparent";
-//         }, 1800);
-
-//         setTimeout(() => {
-//             logo.style.transform = "scale(0)";
-//         }, 2500);
-
-//         setTimeout(() => {
-//             logo.style.display = "none";
-//         }, 3500);
-//     }
-// })
-
-const urlParams = new URLSearchParams(window.location.href);
-alert(urlParams.getAll("loaded"));
-
-window.addEventListener("load", () => {
-    setTimeout(() => {
-        logoNameFirst.style.transform = "translateX(0)";
-    }, 800);
-
-    setTimeout(() => {
-        logoNameSecond.style.transform = "translateX(0)";
-    }, 800);
-
+if (animation === "done") {
+    logo.style.display = "none";
+    cover.style.display = "none";
     projectShape.forEach(element => {
-        element.classList.add("animation");
-        setTimeout(() => {
-            element.classList.remove("animation");
-        }, 1800);
+        element.classList.remove("animation");
     });
+} else {
+    sessionStorage.getItem("animation", "done");
+    window.addEventListener("load", () => {
+        setTimeout(() => {
+            logoNameFirst.style.transform = "translateX(0)";
+        }, 800);
 
-    setTimeout(() => {
-        cover.style.backgroundColor = "transparent";
-    }, 1800);
+        setTimeout(() => {
+            logoNameSecond.style.transform = "translateX(0)";
+        }, 800);
 
-    setTimeout(() => {
-        logo.style.transform = "scale(0)";
-    }, 2500);
+        projectShape.forEach(element => {
+            element.classList.add("animation");
+            setTimeout(() => {
+                element.classList.remove("animation");
+            }, 1800);
+        });
 
-    setTimeout(() => {
-        logo.style.display = "none";
-    }, 3500);
-})
+        setTimeout(() => {
+            cover.style.backgroundColor = "transparent";
+        }, 1800);
+
+        setTimeout(() => {
+            logo.style.transform = "scale(0)";
+        }, 2500);
+
+        setTimeout(() => {
+            logo.style.display = "none";
+        }, 3500);
+    })
+}
+
+
